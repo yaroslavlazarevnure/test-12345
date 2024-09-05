d92fe3f610512afc951123c5e1bcc7df00e95a1e
yaroslavlazarevnure <38326468+yaroslavlazarevnure@users.noreply.github.com>
2024-09-05 17:36:12 +0200

    Initial Commit

---
+++ pr 2.0/pr_2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  pr_2_0App.swift
+//  pr 2.0
+//
+//  Created by Ярослав on 05.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct pr_2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
